ba7f8d85bbbd259f21ab5c92859cc24acf62aba7
Mark Myers <marcusmyers@gmail.com>
2016-08-15 23:53:55 -0400

    Initial commit

---
+++ src/Command.php
@@ -0,0 +1,15 @@
+<?php namespace Mox;
+
+use Symfony\Component\Console\Command\Command as SymfonyCommand;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Command extends SymfonyCommand {
+  public function __construct()
+  {
+    parent::__construct();
+  }
+}
